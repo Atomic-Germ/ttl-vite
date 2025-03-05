753af379a834ccef01a9a8fe2e27257f544ccf3f
Casey Parker <97569476+Atomic-Germ@users.noreply.github.com>
2025-03-05 09:41:45 -0800

    Dev/eslint (#6)
    * modified:   index.html
    	modified:   package.json
    
    * modified:   src-tauri/tauri.conf.json
    	modified:   src/styles.css
    	deleted:    src/ts/calendar.ts
    
    * title bar and button
    
    * config eslint in js
    
    * deleted:    eslint.config.mjs
    	modified:   src/styles.css
    
    * modified:   package.json
    
    * modified:   index.html
    
    * modified:   src/styles.css

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+// eslint.config.js
+export default [
+    {
+        ignores: [
+            "configs/",
+            "build/",
+            "dev-build/",
+            "assets/",
+            "node_modules/",
+            "!src/",
+            "!src/tauri"
+        ]
+    }
+];
